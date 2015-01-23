0bc95a07e786a59cf233be468a2025aa795db94e
Liv Erickson <livi@outlook.com>
2015-01-23 11:22:19 -0800

    Updated the scene

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+
+    var renderer = new THREE.WebGLRenderer();
+    var vrrenderer = new THREE.VRRenderer(renderer, vrHMD);
+
+
     function vrDeviceCallback(vrdevs) {
     for (var i = 0; i < vrdevs.length; ++i) {
       if (vrdevs[i] instanceof HMDVRDevice) {
@@ -21,10 +26,11 @@
       camera = new THREE.PerspectiveCamera(60, 1280 / 800, 0.001, 10);
       camera.position.z = 2;
       scene = new THREE.Scene();
-      var geometry = new THREE.IcosahedronGeometry(1, 1);
+      var geometry = new THREE.TorusKnotGeometry(1, 4, 64, 8, 2, 3, 1);
       var material = new THREE.MeshNormalMaterial();
       mesh = new THREE.Mesh(geometry, material);
       scene.add(mesh);
+      scene.add(camera);
     }
 
     function initRenderer() {
@@ -40,6 +46,13 @@
     function render() {
       requestAnimationFrame(render);
       mesh.rotation.y += 0.01;
+      mesh.rotation.x += 0.01;
       var state = vrHMDSensor.getState();
-        vrrenderer.render(scene, camera);
-      }
+      /** TODO: Identify correct camera orientation
+      camera.quaternion.set(state.orientation.x,
+        state.orientation.y,
+        state.orientation.z,
+        state.orientation.w);
+      **/
+      vrrenderer.render(scene, camera);
+    }
